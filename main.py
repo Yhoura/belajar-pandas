f72bf0f05a497938dce5425531a48431b63b9dd6
wiyono <dewantorowiyono@gmail.com>
2025-07-14 07:04:07 +0700

    first commit to learn pandas

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# %% Ini awal cell
+import pandas as pd
+
+df = pd.DataFrame({
+    "Nama": ["Ayu", "Budi", "Cici"],
+    "Umur": [23, 25, 22]
+})
+
+df  # tampil di interactive window
+
+# %%
+
+
